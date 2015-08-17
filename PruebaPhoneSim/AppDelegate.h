e82bbdb5e5807f8b8db4d35d94c52e43a3e551c8
victor Franco <victorfranco@MacBook-Air-de-victor.local>
2015-08-17 11:33:21 -0500

    Initial Commit

---
+++ PruebaPhoneSim/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PruebaPhoneSim
+//
+//  Created by victor Franco on 17/08/15.
+//  Copyright (c) 2015 grisostomos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PruebaPhoneSim/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PruebaPhoneSim
+//
+//  Created by victor Franco on 17/08/15.
+//  Copyright (c) 2015 grisostomos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
